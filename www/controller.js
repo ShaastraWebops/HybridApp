d9e2a72c28db525085d9adbfd16f8e19051aa2ba
Mohit Kumar <mhtkmr54@gmail.com>
2015-11-11 17:51:59 +0530

    checking for internet connectivity

--- www/controller.js
+++ www/controller.js
@@ -1,6 +1,20 @@
 // This is a JavaScript file
 var module = angular.module('app', ['onsen','ngAnimate']);
 
+module.run(function($window, $rootScope) {
+      $rootScope.online = navigator.onLine;
+      $window.addEventListener("offline", function () {
+        $rootScope.$apply(function() {
+          $rootScope.online = false;
+        });
+      }, false);
+      $window.addEventListener("online", function () {
+        $rootScope.$apply(function() {
+          $rootScope.online = true;
+        });
+      }, false);
+});
+
 module.controller('signupcontroller',function($scope, $http){
     $scope.close = function(){
         modal.close();
